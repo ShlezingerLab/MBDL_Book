0db8bb54a641716a7a7e9538d6b9d512bfce9e51
Elad Sofer <elad.g.sofer@gmail.com>
2023-06-03 18:42:25 +0300

    refactoring l-admm graphs

--- plots.py
+++ plots.py
@@ -1,43 +1,4 @@
 import matplotlib.pyplot as plt
-# import torch, numpy as np
-# import torch.utils.data as Data
-#
-#
-# class SimulatedData(Data.Dataset):
-#     def __init__(self, x, H, s):
-#         self.x = x
-#         self.s = s
-#         self.H = H
-#
-#     def __len__(self):
-#         return self.x.shape[1]
-#
-#     def __getitem__(self, idx):
-#         x = self.x[:, idx]
-#         H = self.H
-#         s = self.s[:, idx]
-#         return x, H, s
-#
-#
-# def create_data_set(H, n, m, k, N=1000, batch_size=512, snr=30, noise_dev=0.5):
-#     # Initialization
-#
-#     x = torch.zeros(n, N)
-#     s = torch.zeros(m, N)
-#     # Create signals
-#     for i in range(N):
-#         # Create a sparsed signal s
-#         index_k = np.random.choice(m, k, replace=False)
-#         peaks = noise_dev * np.random.randn(k)
-#
-#         s[index_k, i] = torch.from_numpy(peaks).to(s)
-#
-#         # X = Hs+w
-#         x[:, i] = H @ s[:, i] + 0.01 * np.random.randn(n)
-#
-#     simulated = SimulatedData(x=x, H=H, s=s)
-#     data_loader = Data.DataLoader(dataset=simulated, batch_size=batch_size, shuffle=True)
-#     return data_loader
 
 
 def plot_observation(x, s):
@@ -78,14 +39,19 @@ def plot_convergence(s_gt, s_hat, errors):
 def plot_admm_vs_ladmm_reconstruction(s_hat_admm, s_hat_ladmm, T, s_gt):
     plt.figure()
     plt.subplot(2, 1, 1)
-    plt.title("L-ADMM K={0}".format(T), fontsize=10)
+    # plt.title("L-ADMM K={0}".format(T), fontsize=10)
     plt.plot(s_hat_ladmm, '.--', label='L-ADMM, K={0}'.format(T), color='r', linewidth=1)
     plt.plot(s_gt, label='sparse signal', color='k')
     plt.xlabel('Index', fontsize=10)
     plt.ylabel('Value', fontsize=10)
     plt.legend()
-    plt.subplot(2, 1, 2)
-    plt.title("ADMM K={0}".format(T))
+    axs1 = plt.subplot(2, 1, 2)
+
+    new_pos = axs1.get_position()
+    new_pos.y0 -= 0.15 * new_pos.y0
+    new_pos.y1 -= 0.15 * new_pos.y1
+    axs1.set_position(pos=new_pos)
+    # plt.title("ADMM K={0}".format(T))
     plt.plot(s_hat_admm, '.--', label='ADMM, K={0}'.format(T), color='r', linewidth=1)
     plt.plot(s_gt, label='sparse signal', color='k')
     plt.xlabel('Index', fontsize=10)
@@ -116,7 +82,12 @@ def plot_admm_vs_admm_1d_reconstruction(s_hat_ladmm, s_hat_admm, max_iter, s_gt,
     plt.ylabel('Value', fontsize=10)
     plt.legend()
 
-    plt.subplot(2, 1, 2)
+    axs1 = plt.subplot(2, 1, 2)
+
+    new_pos = axs1.get_position()
+    new_pos.y0 -= 0.15 * new_pos.y0
+    new_pos.y1 -= 0.15 * new_pos.y1
+    axs1.set_position(pos=new_pos)
 
     plt.plot(s_hat_admm, '.--', label='ADMM, MaxIter={0}'.format(max_iter), color='r', linewidth=1)
     plt.plot(s_gt, label='sparse signal', color='k')
